02c6e3413e2848cc689be4bd9bfd234e1bf8d6b2
Hongyu Xu <43998302+Xuhy0826@users.noreply.github.com>
2021-05-06 10:04:21 +0800

    0506

---
+++ Json.Test/Json.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Json.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonStr = File.ReadAllText("./json.json");
+
+            //(1) Deserialize
+            var obj = Welcome.FromJson(jsonStr);
+
+            //(2) JObject
+            JObject jObject = JObject.Parse(jsonStr);
+            Console.WriteLine(jObject["greeting"]);
+
+            Console.WriteLine(jObject["instructions"][0]);
+
+            Console.ReadLine();
+        }
+    }
+}
